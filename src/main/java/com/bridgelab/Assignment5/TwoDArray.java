d405271a1f08c7e5fb1a66e03cd7867b8436ea1c
atikalg <who.atik@gmail.com>
2022-04-08 20:37:29 +0530

    Added : Printing 2D array program

---
+++ src/main/java/com/bridgelab/Assignment5/TwoDArray.java
@@ -0,0 +1,31 @@
+package com.bridgelab.Assignment5;
+import java.util.Scanner;
+import Utility.Util;
+/**
+ * @author atik
+ * Printing two dimensional array
+ */ 
+public class TwoDArray {
+public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.print("Enter  no of rows you want: ");
+		int row = sc.nextInt();
+		System.out.print("Enter  no of column  you want: ");
+		int column = sc.nextInt();  
+		int [][] matrix = new int[row][column];
+		for (int i = 0; i < row; i++)
+		{
+		    for(int j = 0; j < column; j++) {
+		        System.out.println("Row ["+i+"]:  Column " +j+ " :");
+		    matrix[i][j] = sc.nextInt(); 
+		}
+	}
+		for(int i = 0; i<row; i++) {
+		    for(int j = 0; j<column; j++) {
+		        System.out.print(matrix[i][j]);
+		    }
+		    System.out.println();
+		}		
+	}
+}
